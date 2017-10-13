4f20233fc25cefd2594871bca94c7d742bcc983d
Griff Green <saveoursonics@gmail.com>
2017-10-13 11:10:55 +0200

    Removed Periods and other small comment changes

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -8,22 +8,22 @@ pragma solidity ^0.4.15;
  */
 contract ERC20 {
   
-    /// @dev Returns the total token supply.
+    /// @dev Returns the total token supply
     function totalSupply() public constant returns (uint256 supply);
 
-    /// @dev Returns the account balance of another account with address _owner.
+    /// @dev Returns the account balance of the account with address _owner
     function balanceOf(address _owner) public constant returns (uint256 balance);
 
-    /// @dev Transfers _value amount of tokens to address _to
+    /// @dev Transfers _value number of tokens to address _to
     function transfer(address _to, uint256 _value) public returns (bool success);
 
-    /// @dev Transfers _value amount of tokens from address _from to address _to
+    /// @dev Transfers _value number of tokens from address _from to address _to
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
 
-    /// @dev Allows _spender to withdraw from your account multiple times, up to the _value amount
+    /// @dev Allows _spender to withdraw from the msg.sender's account up to the _value amount
     function approve(address _spender, uint256 _value) public returns (bool success);
 
-    /// @dev Returns the amount which _spender is still allowed to withdraw from _owner.
+    /// @dev Returns the amount which _spender is still allowed to withdraw from _owner
     function allowance(address _owner, address _spender) public constant returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
